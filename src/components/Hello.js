4d17ef627f5a26fdbc9b9e07ea7effb3dab54e8e
Jesse Clark <hello@developerjesse.com>
2020-01-08 16:17:53 +0200

    Fix Hello.js lint issues.

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,13 +1,11 @@
-import React from 'react'
+import React from 'react';
 import './Hello.css';
 
-const Hello = () =>{
-    return(
-        <div className="hello">
-            <p>Hello World, NPM!</p>
-            <p>Update 3</p>
-        </div>
-    );
-} 
+const Hello = () => (
+  <div className="hello">
+    <p>Hello World, NPM!</p>
+    <p>Update 3</p>
+  </div>
+);
 
 export default Hello;
