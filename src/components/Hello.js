518d9d10fd80810a544bc61f6c5a305eaac5091d
Jesse Clark <hello@developerjesse.com>
2020-01-08 13:15:07 +0200

    Add package and updates and then create Hello component.

---
+++ src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Hello.css';
+
+const Hello = () =>{
+    return(
+        <div className="hello">
+            Hello World, NPM!
+        </div>
+    );
+};
+
+export default Hello;
